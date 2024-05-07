64708e746574f28f415c5f8cabea7cce1c16c523
M-Awwab-Khan <mawwabkhank2006@gmail.com>
2024-05-07 11:54:16 +0500

    main server file

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-import keras
+import PIL.Image
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import io
+# from utilities import classify_image
 
-model = keras.models.load_model('cat-vs-dog.keras')
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+@app.post('/predict-image/')
+async def predict_image(file: UploadFile = File(...)):
+    contents = await file.read()
+    pil_image = PIL.Image.open(io.BytesIO(contents))
+    predicted_class, confidence = classify_image(pil_image)
+    return {
+        "class": predicted_class,
+        "confidence": confidence
+    }
